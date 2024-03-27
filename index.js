b680ba1566731455cc6ea23eca51b7eb28ac5fad
cuong_nl <cuonglc@vtdglobal.com>
2024-03-27 14:34:29 +0700

    enable conversion from plain text to cipher text and  from cipher text to plain text

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+var dot = require('dotenv')
+dot.config();
 var express = require('express');
 var AES = require('./aes');
 var app = express();
 
+AES.init(process.env.SECRET_KEY || "Thats my Kung Fu");
+
 app.listen(3000, function () {
-    const keyText = "Thats my Kung Fu";
-    const aes = new AES(keyText)
-    const state = aes.encrypt("Two One Nine Two");
-    aes.decrypt(state);
+    const cipherText = AES.encrypt("Two One Nine Two");
+    AES.decrypt(cipherText);
 });
