b03fbd321f55c6ff11104c6722697876ca331c5a
duongdaoq <duongdaoq@gmail.com>
2025-03-21 15:47:42 +0700

    first commit

--- server/database.js
+++ server/database.js
@@ -1,58 +1,66 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Matkhau@123',
-    database: 'baomat'
+  host: "localhost",
+  user: "root",
+  password: "Duck130603@",
+  database: "aes",
 });
 connection.connect();
 
-const { sampleData } = require('./sample_data');
+const { sampleData } = require("./sample_data");
 
 // setup thuật toán mã hóa
-var AES = require('./aes');
+var AES = require("./aes");
 
 // create table if not exists
 connection.query(
-    'CREATE TABLE IF NOT EXISTS users (citizenIdentificationCard VARCHAR(255) PRIMARY KEY, userName VARCHAR(255), password VARCHAR(255), gender VARCHAR(255), dateOfBirth VARCHAR(255), address VARCHAR(255), phoneNumber VARCHAR(255))'
-    , function (err, results) {
-        if (err) {
-            return console.log(err.message);
-        }
-
-        console.log("Create table users success");
-
-        let isErrorOccurred = false;
-
-        // insert sample data
-        sampleData.forEach(user => {
-            const { userName, password, citizenIdentificationCard, gender, dateOfBirth, address, phoneNumber } = user;
-
-            const encryptedData = {};
-            encryptedData.encryptedUserName = AES.encrypt(userName, password);
-            encryptedData.encryptedPassword = AES.encrypt(password, password);
-            encryptedData.encryptedGender = AES.encrypt(gender, password);
-            encryptedData.encryptedDateOfBirth = AES.encrypt(dateOfBirth, password);
-            encryptedData.encryptedAddress = AES.encrypt(address, password);
-            encryptedData.encryptedPhoneNumber = AES.encrypt(phoneNumber, password);
-
-            // insert user to database ( mã hóa dữ liệu trước khi insert vào db )
-            const query = `INSERT INTO users (citizenIdentificationCard, userName, password, gender, dateOfBirth, address, phoneNumber) VALUES ('${citizenIdentificationCard}', '${encryptedData.encryptedUserName}', '${encryptedData.encryptedPassword}', '${encryptedData.encryptedGender}', '${encryptedData.encryptedDateOfBirth}', '${encryptedData.encryptedAddress}', '${encryptedData.encryptedPhoneNumber}')`;
-
-            connection.query(query, function (err) {
-                if (err) {
-                    isErrorOccurred = true;
-                    return;
-                }
-            });
-        });
-
-        if (!isErrorOccurred) {
-            console.log("Insert sample data success");
-        }
+  "CREATE TABLE IF NOT EXISTS users (citizenIdentificationCard VARCHAR(255) PRIMARY KEY, userName VARCHAR(255), password VARCHAR(255), gender VARCHAR(255), dateOfBirth VARCHAR(255), address VARCHAR(255), phoneNumber VARCHAR(255))",
+  function (err, results) {
+    if (err) {
+      return console.log(err.message);
     }
+
+    console.log("Create table users success");
+
+    let isErrorOccurred = false;
+
+    // insert sample data
+    sampleData.forEach((user) => {
+      const {
+        userName,
+        password,
+        citizenIdentificationCard,
+        gender,
+        dateOfBirth,
+        address,
+        phoneNumber,
+      } = user;
+
+      const encryptedData = {};
+      encryptedData.encryptedUserName = AES.encrypt(userName, password);
+      encryptedData.encryptedPassword = AES.encrypt(password, password);
+      encryptedData.encryptedGender = AES.encrypt(gender, password);
+      encryptedData.encryptedDateOfBirth = AES.encrypt(dateOfBirth, password);
+      encryptedData.encryptedAddress = AES.encrypt(address, password);
+      encryptedData.encryptedPhoneNumber = AES.encrypt(phoneNumber, password);
+
+      // insert user to database ( mã hóa dữ liệu trước khi insert vào db )
+      const query = `INSERT INTO users (citizenIdentificationCard, userName, password, gender, dateOfBirth, address, phoneNumber) VALUES ('${citizenIdentificationCard}', '${encryptedData.encryptedUserName}', '${encryptedData.encryptedPassword}', '${encryptedData.encryptedGender}', '${encryptedData.encryptedDateOfBirth}', '${encryptedData.encryptedAddress}', '${encryptedData.encryptedPhoneNumber}')`;
+
+      connection.query(query, function (err) {
+        if (err) {
+          isErrorOccurred = true;
+          return;
+        }
+      });
+    });
+
+    if (!isErrorOccurred) {
+      console.log("Insert sample data success");
+    }
+  }
 );
 
 module.exports = {
-    connection
-}
+  connection,
+};
